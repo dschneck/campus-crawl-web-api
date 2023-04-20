d6fd6ea37d9f4019bed520f1448562ce716514be
dschneck <david.schneckster@hotmail.com>
2023-04-20 18:29:27 -0400

    wellthen

--- src/DataAccess/Entities/RSO.cs
+++ src/DataAccess/Entities/RSO.cs
@@ -14,5 +14,7 @@ namespace DataAccess.Entities {
         public string Name { get; set; }
 
         public string Status { get; set; }
+
+        public string Description { get; set; }
     }
 }
--- src/DataAccess/dbscripts/DDL/rso.sql
+++ src/DataAccess/dbscripts/DDL/rso.sql
@@ -2,6 +2,7 @@ CREATE TABLE RSO (
     Id STRING(36) NOT NULL,
     UniversityId STRING(36) NOT NULL,
     Status STRING(10) NOT NULL,
+    Description STRING(100) NOT,
     Name STRING(20) NOT NULL,
     FOREIGN KEY(UniversityId) REFERENCES University(Id)
 ) PRIMARY KEY(Id);
