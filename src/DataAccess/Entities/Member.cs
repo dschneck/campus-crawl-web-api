43e7bc6a9215221179ce09a2a695c99838affd5b
dschneck <david.schneckster@hotmail.com>
2023-04-21 19:33:55 -0400

    astor

--- src/DataAccess/Entities/Member.cs
+++ src/DataAccess/Entities/Member.cs
@@ -1,14 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace DataAccess.Entities {
     [Table("Member")]
     public class Member {
+        [Key]
         [ForeignKey("User")]
         public string UserId { get; set; }
         [JsonIgnore]
         public User User { get; set; }
 
+        [Key]
         [ForeignKey("RSO")]
         public string RSOId { get; set; }
         [JsonIgnore]
